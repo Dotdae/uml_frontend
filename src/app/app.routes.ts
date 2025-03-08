44aa6a6e77538ced47eb4b3b4cb354ba73ee40f1
Adrieliyo <AngelRodriguez2003@Outlook.com>
2025-03-08 00:44:43 -0700

    Add authentication and home page components with styles and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Componentes Principales
+import { HomePageComponent } from './presentation/pages/home-page/home-page.component';
+import { AuthComponent } from './presentation/pages/auth/auth.component';
+import { SignInComponent } from './presentation/pages/auth/sign-in/sign-in.component';
+import { SignUpComponent } from './presentation/pages/auth/sign-up/sign-up.component';
+/*
+    Faltan los componentes de:
+    - Recuperar contraseña
+    - Confirmar correo
+    - Cambiar contraseña
+*/
+
+// Aquí se agregarían más rutas de las páginas faltantes de la aplicación
+
+export const routes: Routes = [
+
+    // Página de inicio
+    {
+        path: "", component: HomePageComponent, title: "UMLForge - De ideas a código"
+    },
+
+    // Inicio de sesión
+    {
+        path: "auth", component: AuthComponent, title: "UMLForge - Inicio de sesión",
+        children: [
+            {
+                path: "", component: SignInComponent
+            },
+            {
+                path: "sign-up", component: SignUpComponent, title: "UMLForge - Registrarse"
+            }
+        ]
+    }
+
+    // Aquí se agregarían las rutas de las páginas faltantes
+
+];
