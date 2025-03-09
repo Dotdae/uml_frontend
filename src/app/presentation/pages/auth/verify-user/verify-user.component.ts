c6bd4460d7743110503918e7d24d7c2c5ced3326
Adrieliyo <AngelRodriguez2003@Outlook.com>
2025-03-09 01:16:57 -0700

    Enhance user verification page with success/error messages and navigation button

--- src/app/presentation/pages/auth/verify-user/verify-user.component.ts
+++ src/app/presentation/pages/auth/verify-user/verify-user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+// import { VerifyAccountUseCase } from '@application/usecases/user/verify-account.usecase';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-verify-user',
@@ -7,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrl: './verify-user.component.css'
 })
 export class VerifyUserComponent {
+  VerificationError: boolean = false;
+  
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    // private verifyAccountUseCase: VerifyAccountUseCase
+  ){ }
 
+  // ngOnInit() {
+  //   const token = this.route.snapshot.params['token'];
+  //   if (token) {
+  //     this.verifyAccountUseCase.execute(token).subscribe({
+  //       next: () => {
+  //         this.VerificationError = false;
+  //       },
+  //       error: (error) => {
+  //         console.error('Error verifying account:', error);
+  //         this.VerificationError = true;
+  //       }
+  //     });
+  //   } else {
+  //     this.VerificationError = true;
+  //   }
+  // }
+
+  goBackToLog(){
+    this.router.navigate(['/auth'])
+  }
 }
